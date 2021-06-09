26871d6e3678b2eff6f95bdd4d54b80f7d82ae56
Harrison Koll <70385098+HKollU@users.noreply.github.com>
2021-06-09 17:17:00 -0500

    Create BScraper.py

---
+++ BScraper.py
@@ -0,0 +1,21 @@
+import requests,sys,webbrowser
+from bs4 import BeautifulSoup
+
+URL = 'https://www.newegg.com/p/1HJ-0113-00008?Item=9SIAGDWD026815&quicklink=t>
+if len(sys.argv) > 1:
+        URL = sys.argv[1] 
+
+while True:
+        page = requests.get(URL)
+
+        soup = BeautifulSoup(page.content,'html.parser')
+        results = soup.find(id='ProductBuy')
+        themessage = results.find_all('button',class_='btn btn-primary btn-wid>
+        for themessage in themessage:
+                text_elem = themessage.text
+                title = themessage.title
+                print(text_elem)
+                if 'Add' in text_elem:
+                        print('STOCK FOUND!: ' + URL)
+                        webbrowser.open_new(URL)
+                        quit()
